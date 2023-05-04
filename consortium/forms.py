724d248b781185421ac7cff2bc5272140c41c47f
THEleigend <120776504+THEleigend@users.noreply.github.com>
2023-05-04 12:14:33 +0800

    update

--- consortium/forms.py
+++ consortium/forms.py
@@ -1,5 +1,5 @@
 from django import forms  
-from consortium.models import CMI  
+from consortium.models import CMI, Consortium  
 
 
 class CMIForm(forms.ModelForm):  
@@ -22,5 +22,30 @@ class CMIForm(forms.ModelForm):
             'email': 'Email',
             'url': 'URL',
             'remarks': 'Remarks (Inactive)'
+        }
+
+
+class ConsortiumForm(forms.ModelForm):  
+
+    class Meta:  
+        model = Consortium  
+        exclude = ('created_by', 'modified_by')
+        fields = '__all__'
+        labels = {
+            'consortium_name': 'Consortium Name',
+            'consortium_code': 'Consortium Code',
+            'consortium_address' : 'Address',
+            'geolat': 'Latitude',
+            'geolong': 'Longitude',
+            'consortium_logo': 'Consortium Logo',
+            'mission': 'Mission',
+            'vision': 'Vision',
+            'consortium_desc': 'Description',
+            'consortium_objectives': 'Objectives',
+            'url': 'URL',
+            'fb_url': 'Facebook',
+            'yt_url': 'Youtube',
+            'telno': 'Telephone Number',
+            'email': 'Email Address',
         }
 
